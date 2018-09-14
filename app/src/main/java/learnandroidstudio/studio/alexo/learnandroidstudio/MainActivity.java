e61b15f12449b6b75269fe773083ee525e4b0569
alexolirib <alexolirib@gmail.com>
2018-09-14 10:22:35 -0300

    Feito app de frase do dia

--- app/src/main/java/learnandroidstudio/studio/alexo/learnandroidstudio/MainActivity.java
+++ app/src/main/java/learnandroidstudio/studio/alexo/learnandroidstudio/MainActivity.java
@@ -10,20 +10,33 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnJogar;
-    private TextView lblResult;
+//    private Button btnJogar;
+//    private TextView lblResult;
+
+    //nova frase dia
+    private TextView lblNovaFrase;
+    private Button botaoNovaFrase;
+    private String[] frases = {
+            "Você pode, você é capaz",
+            "Se você traçar metas absurdamente altas e falhar, seu fracasso será muito melhor que o sucesso de todos\" – James Cameron, cineasta",
+            "O sucesso normalmente vem para quem está ocupado demais para procurar por ele",
+            "A vida é melhor para aqueles que fazem o possível para ter o melhor",
+            "Os empreendedores falham, em média, 3,8 vezes antes do sucesso final. O que separa os bem-sucedidos dos outros é a persistência"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_adivinha);
+       // setContentView(R.layout.activity_adivinha);
+        setContentView(R.layout.activity_frasedodia);
 
-        btnJogar = findViewById(R.id.btnJogarId);
-        lblResult = findViewById(R.id.lblResultId);
+        //adivinha
+//
+//        btnJogar = findViewById(R.id.btnJogarId);
+//        lblResult = findViewById(R.id.lblResultId);
 
 //        lblResult.setText("troquei o valor");
-        btnJogar.setOnClickListener(new View.OnClickListener() {
+       /* btnJogar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //gerar numero randomico
@@ -32,6 +45,19 @@ public class MainActivity extends AppCompatActivity {
                 int numeroAleatorio = randomico.nextInt(10);
                 lblResult.setText("Número escolhido: " + numeroAleatorio);
                 //lblResult.setText("Texto alterado");
+            }
+        });*/
+
+        //nova frase do dia
+        lblNovaFrase = findViewById(R.id.textoNovaFraseId);
+        botaoNovaFrase = findViewById(R.id.btnNovaFraseId);
+
+        botaoNovaFrase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random randomico = new Random();
+                int numAleatorio = randomico.nextInt(frases.length);
+                lblNovaFrase.setText(frases[numAleatorio]);
             }
         });
     }
