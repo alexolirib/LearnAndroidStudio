5c4b97f17c9edbdbbfbb29d4e1cd12c4aae09e6a
alexolirib <alexolirib@gmail.com>
2018-09-18 13:28:24 -0300

    iniciando construção interface netflix

---
+++ app/src/main/java/learnandroidstudio/studio/alexo/learnandroidstudio/NetflixInterfaceActivity.java
@@ -0,0 +1,13 @@
+package learnandroidstudio.studio.alexo.learnandroidstudio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NetflixInterfaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_netflix_interface);
+    }
+}
